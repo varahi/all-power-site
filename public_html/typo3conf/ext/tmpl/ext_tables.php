8d131270d86400ae7e3bec98dd17a81a5d2a0722
Dmitry Vasilev <dmitry@t3dev.ru>
2019-06-17 11:36:46 +0300

    Feature #268 - change web directory from htdocs to public_html

---
+++ public_html/typo3conf/ext/tmpl/ext_tables.php
@@ -0,0 +1,40 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+
+/**
+ * Add TypoScript
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Basis tmpl extension');
+
+/**
+ * Page TS Config
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tmpl/Configuration/TsConfig/page.tsconfig">');
+
+/**
+ * Add some basic User TS Config
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tmpl/Configuration/TsConfig/user.tsconfig">');
+
+/**
+ * Add rte_ckeditor custom config
+ */
+//$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['custom'] = 'EXT:tmpl/Configuration/RTE/Custom.yaml';
+
+/*
+call_user_func(
+    function()
+    {
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            'T3Dev.Tmpl',
+            'Tmpl',
+            'Tmpl'
+        );
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('tmpl', 'Configuration/TypoScript', 'Tmpl');
+
+    }
+);
+*/
