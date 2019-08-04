432f4d505ed63badd8880594412171c8ae979ddc
Dmitry Vasilev <dmitry@t3dev.ru>
2019-08-04 17:56:22 +0300

    Add parallax effect

--- new_design/Prototype/Javascript/main.js
+++ new_design/Prototype/Javascript/main.js
@@ -20,5 +20,38 @@ $(document).ready(function() {
 				margin: 20
 			}
 		}
-	})
+	});
+});
+
+
+//Parallax
+/*
+$(document).ready(function(){
+	$window = $(window);
+	$('section[data-type="background"]').each(function(){
+		var $bgobj = $(this); // Назначаем объект
+		$(window).scroll(function() {
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+			var coords = '50% '+ yPos + 'px';
+			$bgobj.css({ backgroundPosition: coords });
+		});
+	});
+});
+
+document.createElement("article");
+document.createElement("section");
+ */
+
+
+$(document).ready(function(){
+	$('section[data-type="background"]').each(function(){
+		var $bgobj = $(this); // создаем объект
+		$(window).scroll(function() {
+			var yPos = -($window.scrollTop() / $bgobj.data('speed')); // вычисляем коэффициент
+			// Присваиваем значение background-position
+			var coords = 'center '+ yPos + 'px';
+			// Создаем эффект Parallax Scrolling
+			$bgobj.css({ backgroundPosition: coords });
+		});
+	});
 })
